fe54380ed603fece0c58c9b204a0551d93f4f9a9
lbCyber <paul@lintbox.com>
2019-10-21 20:18:14 -0400

    Movie list transitions cards in

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import MovieCard from './local/movieCard'
 import Languages from './local/languages'
 import './css/style.css'
+import { CSSTransition } from 'react-transition-group'
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +13,8 @@ class App extends Component {
     this.state = {
       loading: true,
       apiData: [],
-      languages: Languages
+      languages: Languages,
+      ready: false
     }
   }
 
@@ -31,16 +33,12 @@ class App extends Component {
       return language.iso_639_1 === lang;
     }
     const output = this.state.languages.find(langCompare); //Find the language
-    if ((output["name"] === '') || (output["name"] === output["english_name"])) {
-      return `${output["english_name"]}` //Return English name if 'name' is blank or if 'name' and 'english_name' match
-    } else {
-      return `${output["english_name"]} (${output["name"]})` //Return English name
-    }
+    return `${output["english_name"]}` //Return English name
   }
 
   componentDidMount() {
     axios({
-      url: 'https://api.themoviedb.org/3/list/124382',
+      url: 'https://api.themoviedb.org/3/list/124441',
       method: 'GET',
       dataType: 'json',
       params: {
@@ -49,26 +47,37 @@ class App extends Component {
     }).then(response => {
       this.setState({ apiData: response.data.items })
     }).catch(error => {  // If nothing matched, something went wrong on your end!
-      console.log(error)
       this.warningFire(`Something went wrong on our end! Please wait a moment, and try your search again!`)
     }).finally(() => {
-      this.setState({ loading: false })
+      this.setState({
+        loading: false,
+        ready: true
+      })
     })
   }
   render() {
     return (
       <React.Fragment>
-        {this.state.loading ?
-          <LoadingModal />
-          : null
-        }
-        <div className="movieGrid">
-          {
-            this.state.apiData.map((movie, key) => {
-              return (
-                <MovieCard moviePick={movie} language={this.pickLanguage(movie.original_language)} key={key} />
-              )
-            })
+          <CSSTransition
+            in={this.state.loading}
+            timeout={5000}
+            classNames="loadFade"
+            unmountOnExit
+          >
+            <LoadingModal />
+          </CSSTransition>
+        <div className="wrapper">
+          {this.state.loading === false ?
+            <div className="movieGrid">
+              {
+                this.state.apiData.map((movie, key) => {
+                  return (
+                    <MovieCard moviePick={movie} language={this.pickLanguage(movie.original_language)} key={key} cardNumber={key} ready={this.state.ready} />
+                  )
+                })
+              }
+            </div>
+            : null
           }
         </div>
       </React.Fragment>
--- src/local/movieCard.js
+++ src/local/movieCard.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group'
+import './../css/style.css'
 
 class MovieCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      loaded: false
     }
+  }
+
+  componentWillMount() {
+    const img = new Image();
+    img.src = `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${this.props.moviePick.poster_path}`
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loaded: true
+      })
+    },
+      this.props.cardNumber * 75
+    )
   }
 
   render() {
     return (
-      <div className="card">
-        <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${this.props.moviePick.poster_path}`} alt="" />
-        <h4>{`${this.props.moviePick.title} (${this.props.moviePick.release_date.slice(0, 4)})`}</h4>
-        <h5>Language: {this.props.language}</h5>
-        <h6>Rating: {this.props.moviePick.vote_average}/10</h6>
-      </div>
+      <React.Fragment>
+          {this.props.ready ?
+            <CSSTransition
+              in={this.state.loaded}
+              timeout={100}
+              classNames="cardLoadFade">
+              <div className="card" id={`card${this.props.cardNumber}`}>
+                <div className="cardTitle">
+                  <h4>{`${this.props.moviePick.title}`}</h4>
+                  <h4>{`(${this.props.moviePick.release_date.slice(0, 4)})`}</h4>
+                </div>
+                <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${this.props.moviePick.poster_path}`} alt={`Movie poster for ${this.props.moviePick.title}`} />
+                <h5>Language: {this.props.language}</h5>
+                <h6><a href={`https://www.themoviedb.org/movie/${this.props.moviePick.id}`} target="_blank" rel="noopener noreferrer">TMDB Rating:</a> {this.props.moviePick.vote_average}/10</h6>
+              </div>
+            </CSSTransition>
+            : null
+          }
+      </React.Fragment>
     )
   }
 }
